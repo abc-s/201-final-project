4a2ae7c026338f1f56bd775562fd9e42613964fa
Billy Bunn <williammbunn@gmail.com>
2019-01-28 16:48:55 -0800

    Adds code org comments and event listener

--- js/app.js
+++ js/app.js
@@ -1,36 +1,67 @@
 'use strict';
 
-//Declare global variables
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// DATA
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// DOM ACCESS VARIABLES
+var buttonElement = document.getElementById('add-list');
+var inputElement = document.getElementById('new-title');
+
+//OBJECT CONSTRUCTOR VARIABLES
 var List.allLists = [];
 
-//Function to render the initial page
-function renderInitialPage(){
-    //if there's local storage, redirect to lists.html
-}
-
-//Create list constructor function
-function List(listTitle, taskList){
+// LIST CONSTRUCTOR FUNCTION
+function List(listTitle, taskList) {
     this.listTitle = listTitle;
     this.taskList = taskList;
     List.allLists.push(this);
+    console.log('created new List instance');
 };
 
-List.prototype.addTask(userText){
+// LIST 'ADD TASK' PROTOTYPE
+List.prototype.addTask(userText) {
     var task = new Task(userText);
     this.taskList.push(task);
+    console.log('ran .addTask()');
 };
 
-//Create new task constructor function
-function Task(userText){
+// TASK CONSTRUCTOR FUNCTION
+function Task(userText) {
     this.userText = userText;
     this.checked = false;
     this.editing = false;
     this.removed = false;
+    console.log('created new Task instance');
 };
 
-function handleNewList(event){
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// FUNCTION DECLARATIONS
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// Ordered with a stepdown approach. Higher level functions are on top and lower levels below.
+
+// RENDERS CORRECT PAGE
+function renderInitialPage() {
+    //if there's local storage, redirect to lists.html
+}
+
+// BUTTON CLICK EVENT HANDLER
+function handleNewList(submit) {
+    console.log('event.target:', event.target);
+
+    event.preventDefault();
+
+    var listTitle = inputElement.value;
     new List(listTitle, []);
     //var listTitle = event target name?
     //redirect to lists.html
 }
 
+// BUTTON CLICK EVENT LISTENER
+buttonElement.addEventListener('submit', handleNewList);
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// FUNCTION INVOCATIONS
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+renderInitialPage();
