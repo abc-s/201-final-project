ac2002337198344893e8ec937538cdd3fe341e7e
sabdi21 <sabdi21@hotmail.com>
2019-01-29 11:24:12 -0800

    created a function for the li element

--- js/lists.js
+++ js/lists.js
@@ -11,6 +11,24 @@ var addTaskButtonElement = document.getElementById('add-task');
 var incompleteUlElement = document.getElementById('incomplete-list');
 var completeUlElement = document.getElementById('complete-list');
 
+
+task.prototype.createLi = function() {
+// Create DOM Variables
+var listElement = document.createElement('li');
+var labelElement = document.crateElement('label');
+var inputElement = document.createElement('input');
+var buttonElement = document.createElement('button');
+
+// Add content & HTML attributes to variables
+inputElement.innerHTML = this.userText
+inputElement.type = 'checkbox';
+buttonElement.innerHTML = 'X'
+labelElement.appendChild(inputElement);
+labelElement.appendChild(buttonElement);
+listElement.appendChild(labelElement);
+
+// return listElement
+}
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // FUNCTION DECLARATIONS
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
@@ -45,6 +63,8 @@ function appendList(listElement, listItem) {
 
   listElement.appendChild(listItem);
 }
+
+
 
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // FUNCTION INVOCATIONS
