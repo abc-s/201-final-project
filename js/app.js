a023e9de3239a537cab5f565c981bce3f4a1fcc2
amjcurtis <curtis.andrewm@gmail.com>
2019-01-28 18:14:30 -0800

    Add functions for list.html

--- js/app.js
+++ js/app.js
@@ -1,12 +1,16 @@
 'use strict';
 
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // DATA
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 // DOM ACCESS VARIABLES
 var buttonElement = document.getElementById('add-list');
 var inputElement = document.getElementById('new-title');
+var textInputElement = document.getElementById('text-input');
+var addTaskButtonElement = document.getElementById('add-task');
+var incompleteUlElement = document.getElementById('incomplete-list');
+var completeUlElement = document.getElementById('complete-list');
 
 //OBJECT CONSTRUCTOR VARIABLES
 List.allLists = [];
@@ -35,9 +39,9 @@ function Task(userText) {
   console.log('created new Task instance');
 }
 
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // FUNCTION DECLARATIONS
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // Ordered with a stepdown approach. Higher level functions are on top and lower levels below.
 
 // RENDERS CORRECT PAGE
@@ -59,8 +63,37 @@ function handleNewList(event) {
 // BUTTON CLICK EVENT LISTENER
 buttonElement.addEventListener('click', handleNewList);
 
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// FUNCTIONS FOR LIST.HTML 
+
+// SORT AND RENDER TASKS
+function renderTasks() {
+    for (var i = 0; i < List.allLists[0].taskList.length; i++) {
+        if (!List.allLists[0].taskList[i].complete) {
+            // Change out fixed array index '[0]' and replace with different variable if we implement stretch goal of allowing multiple lists 
+            appendList(incompleteUlElement, List.allLists[0].taskList[i]);
+        } else {
+            appendList(completeUlElement, List.allLists[0].taskList[i]); // Change out '[0]' here too if we end up allowing multiple lists
+        }
+    }
+}
+
+// EVENT HANDLER FOR 'ADD TASK' BUTTON CLICK
+function handleAddTask(event) {
+    var newTask = List.addTask(textInputElement.value); // Get text from input field 
+    appendList(incompleteUlElement, newTask);
+}
+
+// ADD LIST ELEMENT TO DOM
+function appendList(listElement, listItem) {
+    // NEED TO CREATE 'LI' ELEMENT AND APPEND THAT TO DOM
+
+    listElement.appendChild(listItem);
+}
+
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // FUNCTION INVOCATIONS
-// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 renderInitialPage();
